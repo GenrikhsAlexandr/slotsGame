0dd863cad586a224287f202a2b6873d5b215b23b
aleksandr.genrikhs <aleksandr.genrikhs@gmail.com>
2023-07-23 15:05:31 +0300

    Initial commit

---
+++ app/src/main/java/com/genrikhs/slotsgame/PrimaryFragment.kt
@@ -0,0 +1,31 @@
+package com.genrikhs.slotsgame
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PrimaryFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PrimaryFragment()
+    }
+
+    private lateinit var viewModel: PrimaryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_primary, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PrimaryViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/genrikhs/slotsgame/PrimaryViewModel.kt
@@ -0,0 +1,7 @@
+package com.genrikhs.slotsgame
+
+import androidx.lifecycle.ViewModel
+
+class PrimaryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
